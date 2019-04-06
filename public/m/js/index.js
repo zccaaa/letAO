ad16412f48a263d519dc32b67da21190fd556c55
zccaaa <137291217@qq.com>
2019-04-06 09:15:16 +0800

    第一次乐淘提交

---
+++ public/m/js/index.js
@@ -0,0 +1,9 @@
+var slider = mui("#slider");
+
+slider.slider({
+    interval: 2000
+});
+// 初始化区域滚动
+mui('.mui-scroll-wrapper').scroll({
+	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+});
